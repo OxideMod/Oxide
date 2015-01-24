03dfe6ca763de370aa847d8f8881026df792d2b9
strykes <renebvip@hotmail.fr>
2015-01-24 22:30:19 +0100

    rust.ForcePlayerPosition & rust.PrivateBindingFlag

--- Oxide.Ext.Rust/Libraries/Rust.cs
+++ Oxide.Ext.Rust/Libraries/Rust.cs
@@ -108,6 +108,32 @@ namespace Oxide.Rust.Libraries
         }
 
         /// <summary>
+        /// Force client to teleport to position
+        /// </summary>
+        /// <param name="player"></param>
+        /// <param name="destination"></param>
+        [LibraryFunction("ForcePlayerPosition")]
+        public void ForcePlayerPosition(BasePlayer player, float x, float y, float z)
+        {
+            var position = player.transform.position;
+            position.x = x;
+            position.y = y;
+            position.z = z;
+            player.transform.position = position;
+            player.ClientRPC(null, player, "ForcePositionTo", new object[] { position });
+            player.TransformChanged();
+        }
+        
+        /// <summary>
+        /// Get a private bindingflag
+        /// </summary>
+        [LibraryFunction("PrivateBindingFlag")]
+        public void PrivateBindingFlag()
+        {
+            return BindingFlags.NonPublic;
+        }
+        
+        /// <summary>
         /// Converts a string into a quote safe string
         /// </summary>
         /// <param name="str"></param>
