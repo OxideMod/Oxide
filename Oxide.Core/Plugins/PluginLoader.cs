2524f3d216b6e15087f8734eb40cba8014cd0260
bawNg <bawng@intoxicated.co.za>
2015-01-27 01:03:13 +0200

    Implemented prerequisites for CSharp plugin extension Exposed Oxide directory paths from core Added TempDirectory to root config Added a LogInfo wrapper Added thread-safe NextTick method Support for asynchronous plugin loading LoadAllPlugins should wait for all asynchronous plugins to finish loading Core will now ask plugin loaders to reload already loaded plugins ReloadPlugin will attempt to unload already loaded plugins Exposed hooks to classes derived from CSPlugin Exposed Plugin.RaiseError to other classes Added additional exception information to CallHook error handling Made defining a LoadDefaultConfig method in plugins optional

--- Oxide.Core/Plugins/PluginLoader.cs
+++ Oxide.Core/Plugins/PluginLoader.cs
@@ -9,6 +9,11 @@ namespace Oxide.Core.Plugins
     public abstract class PluginLoader
     {
         /// <summary>
+        /// Stores the names of plugins which are currently loading asynchronously
+        /// </summary>
+        public List<string> LoadingPlugins = new List<string>();
+
+        /// <summary>
         /// Scans the specified directory and returns a set of plugin names for plugins that this loader can load
         /// </summary>
         /// <param name="directory"></param>
@@ -22,5 +27,15 @@ namespace Oxide.Core.Plugins
         /// <param name="name"></param>
         /// <returns></returns>
         public abstract Plugin Load(string directory, string name);
+
+        /// <summary>
+        /// Reloads a plugin given the specified name, implemented by plugin loaders which support reloading plugins
+        /// </summary>
+        /// <param name="directory"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public virtual void Reload(string directory, string name)
+        {
+        }
     }
 }
