068fe1e2538b6b7c30da527874826d7c84045427
Luke Spragg <luke@spragg.me>
2015-02-09 23:13:46 -0500

    Add PrintToChat method for 7 Days to Die thanks to @bawNg

--- Oxide.Ext.SevenDays/SevenDaysPlugin.cs
+++ Oxide.Ext.SevenDays/SevenDaysPlugin.cs
@@ -2,6 +2,25 @@
 {
     public abstract class SevenDaysPlugin : CSharpPlugin
     {
+        GameManager _gameManager;
+        protected GameManager gameManager
+        {
+            get
+            {
+                if (_gameManager == null)
+                    _gameManager = UnityEngine.Object.FindObjectOfType<GameManager>();
+                return _gameManager;
+            }
+        }
 
+        protected void PrintToChat(string message)
+        {
+            PrintToChat("", message);
+        }
+
+        protected void PrintToChat(string name, string message)
+        {
+            gameManager.SendChatMessage(message, -1, name);
+        }
     }
 }
