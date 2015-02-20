OxideMod/Oxide

commit 3f58ff834c1388e0b020e61beddb442cf1cde586

Added rust.RunServerCommand and userid arg for chat functions - rust.RunServerCommand("ownerid", steamId, player.displayName) - rust.BroadcastChat("SERVER", "Hello world!", "76561197960634567") - rust.SendChatMessage(player, "SERVER", "Hello world!", "76561197960634567")

Oxide.Ext.Rust/Libraries/Rust.cs
@@ -70,21 +70,33 @@ namespace Oxide.Rust.Libraries
         }
 
         /// <summary>
+        /// Runs a server command
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="args"></param>
+        [LibraryFunction("RunServerCommand")]
+        public void RunServerCommand(string command, params object[] args)
+        {
+            ConsoleSystem.Run.Server.Normal(command, args);
+        }
+
+        /// <summary>
         /// Broadcasts a chat message
         /// </summary>
         /// <param name="name"></param>
         /// <param name="message"></param>
+        /// <param name="userid"></param>
         [LibraryFunction("BroadcastChat")]
-        public void BroadcastChat(string name, string message = null)
+        public void BroadcastChat(string name, string message = null, string userid = "0")
         {
             if (message != null)
             {
-                ConsoleSystem.Broadcast("chat.add", 0, string.Format("<color=orange>{0}:</color> {1}", name, message), 1.0);
+                ConsoleSystem.Broadcast("chat.add", userid, string.Format("<color=orange>{0}</color>  {1}", name, message), 1.0);
             }
             else
             {
                 message = name;
-                ConsoleSystem.Broadcast("chat.add", 0, message, 1.0);
+                ConsoleSystem.Broadcast("chat.add", userid, message, 1.0);
             }
         }
 
@@ -94,17 +106,18 @@ namespace Oxide.Rust.Libraries
         /// <param name="player"></param>
         /// <param name="name"></param>
         /// <param name="message"></param>
+        /// <param name="userid"></param>
         [LibraryFunction("SendChatMessage")]
-        public void SendChatMessage(BasePlayer player, string name, string message = null)
+        public void SendChatMessage(BasePlayer player, string name, string message = null, string userid = "0")
         {
             if (message != null)
             {
-                player.SendConsoleCommand("chat.add", 0, string.Format("<color=orange>{0}:</color> {1}", name, message), 1.0);
+                player.SendConsoleCommand("chat.add", userid, string.Format("<color=orange>{0}</color>  {1}", name, message), 1.0);
             }
             else
             {
                 message = name;
-                player.SendConsoleCommand("chat.add", 0, message, 1.0);
+                player.SendConsoleCommand("chat.add", userid, message, 1.0);
             }
         }
 

Oxide.Core/OxideMod.cs
@@ -336,7 +336,7 @@ namespace Oxide.Core
             // Let other plugins know that this plugin has been unloaded
             CallHook("OnPluginUnloaded", new object[] { plugin });
 
-            rootlogger.Write(LogType.Info, "Unloaded plugin {0} (v{1}) by {2}", plugin.Title, plugin.Version, plugin.Author);
+            rootlogger.Write(LogType.Info, "Unloaded plugin {0} v{1} by {2}", plugin.Title, plugin.Version, plugin.Author);
             return true;
         }
 

commit 775a787a7b410cbdbd46ec24e9005191a313b9bc

Fix Rust chat function format which was broken in 3f58ff834c1388e0b020e61beddb442cf1cde586

Oxide.Ext.Rust/Libraries/Rust.cs
@@ -78,7 +78,7 @@ namespace Oxide.Rust.Libraries
         {
             if (message != null)
             {
-                ConsoleSystem.Broadcast("chat.add", userid, string.Format("<color=orange>{0}</color>  {1}", name, message), 1.0);
+                ConsoleSystem.Broadcast("chat.add", userid, string.Format("<color=orange>{0}:</color> {1}", name, message), 1.0);
             }
             else
             {
@@ -99,7 +99,7 @@ namespace Oxide.Rust.Libraries
         {
             if (message != null)
             {
-                player.SendConsoleCommand("chat.add", userid, string.Format("<color=orange>{0}</color>  {1}", name, message), 1.0);
+                player.SendConsoleCommand("chat.add", userid, string.Format("<color=orange>{0}:</color> {1}", name, message), 1.0);
             }
             else
             {