778d8241d1e3c95d1b7e4f4f0109cd3a5d9b84a9
bawNg <bawng@intoxicated.co.za>
2015-03-23 14:28:36 +0200

    Improved RemoteLogger Implemented report queue with retry on failure logic Correctly detect name and version when calling assembly is a plugin Fixed bug when logging without exceptions

--- Oxide.Core/RemoteLogger.cs
+++ Oxide.Core/RemoteLogger.cs
@@ -32,11 +32,23 @@ namespace Oxide.Core
             return new Dictionary<string, string> { { "X-Sentry-Auth", "Sentry " + auth_string } };
         }
 
-        private static Dictionary<string, string> tags = new Dictionary<string, string>()
+        private static Dictionary<string, string> tags = new Dictionary<string, string>
         {
-            { "arch", IntPtr.Size == 4 ? "x86" :  "x64" }
+            { "arch", IntPtr.Size == 4 ? "x86" : "x64" }
         };
 
+        private class QueuedReport
+        {
+            public Dictionary<string, string> Headers;
+            public string Body;
+
+            public QueuedReport(Report report)
+            {
+                Headers = BuildHeaders();
+                Body = JsonConvert.SerializeObject(report);
+            }
+        }
+        
         public class Report
         {
             public string event_id;
@@ -45,37 +57,65 @@ namespace Oxide.Core
             public string platform = "csharp";
             public string culprit;
             public string release = OxideMod.Version.ToString();
-            // ext tags: rust protocols, SystemInfo.operatingSystem
-            // ext fields: server_name
             public Dictionary<string, string> tags = RemoteLogger.tags;
             public Dictionary<string, string> modules;
             public Dictionary<string, string> extra;
 
-            public Report(string level, Assembly assembly, string culprit, string message, string exception=null)
+            private Dictionary<string, string> headers;
+
+            public Report(string level, string culprit, string message, string exception=null)
             {
+                this.headers = BuildHeaders();
                 this.level = level;
                 this.message = message.Length > 1000 ? message.Substring(0, 1000) : message;
                 this.event_id = this.message.GetHashCode().ToString();
                 this.culprit = culprit;
-                var extension_type = assembly.GetTypes().FirstOrDefault(t => t.BaseType == typeof(Extensions.Extension));
-                var extension = Interface.Oxide.GetAllExtensions().FirstOrDefault(e => e.GetType() == extension_type);
-                var assembly_version = extension != null ? extension.Version.ToString() : string.Empty;
-                this.modules = new Dictionary<string, string> { { assembly.GetName().Name, assembly_version } };
                 if (exception != null)
                 {
                     var exception_lines = exception.Split('\n').Take(31).Select(line => line.Trim(' ', '\r', '\n').Replace('\t', ' '));
                     extra = new Dictionary<string, string> { { "exception", string.Join("\\n", exception_lines.ToArray()) } };
                 }                
             }
+
+            public void DetectModules(Assembly assembly)
+            {
+                var assembly_name = assembly.GetName().Name;
+                string assembly_version = string.Empty;
+                var extension_type = assembly.GetTypes().FirstOrDefault(t => t.BaseType == typeof(Extensions.Extension));
+                if (extension_type != null)
+                {
+                    var extension = Interface.Oxide.GetAllExtensions().FirstOrDefault(e => e.GetType() == extension_type);
+                    if (extension != null) assembly_version = extension.Version.ToString();
+                }
+                else
+                {
+                    var plugin_type = assembly.GetTypes().FirstOrDefault(t => IsTypeDerivedFrom(t, typeof(Plugins.Plugin)));
+                    if (plugin_type != null)
+                    {
+                        var plugin = Interface.Oxide.RootPluginManager.GetPlugin(plugin_type.Name);
+                        if (plugin != null)
+                        {
+                            assembly_name = "Plugins." + plugin.Name;
+                            assembly_version = plugin.Version.ToString();
+                        }
+                    }
+                }
+                this.modules = new Dictionary<string, string> { { assembly_name, assembly_version } };
+            }
+
+            private bool IsTypeDerivedFrom(Type type, Type base_type)
+            {
+                while (type != null && type != base_type)
+                    if ((type = type.BaseType) == base_type) return true;
+                return false;
+            }
         }
 
-        private static WebRequests webrequests;
-
-        static RemoteLogger()
-        {
-            webrequests = Interface.Oxide.GetLibrary<WebRequests>("WebRequests");
-        }
-
+        private static Timer timers = Interface.Oxide.GetLibrary<Timer>("Timer");
+        private static WebRequests webrequests = Interface.Oxide.GetLibrary<WebRequests>("WebRequests");
+        private static List<QueuedReport> queuedReports = new List<QueuedReport>();
+        private static bool submittingReports;
+        
         public static void SetTag(string name, string value)
         {
             tags[name] = value;
@@ -83,39 +123,62 @@ namespace Oxide.Core
 
         public static void Debug(string message)
         {
-            SendReport("debug", Assembly.GetCallingAssembly(), GetCurrentMethod(), message);
+            EnqueueReport("debug", Assembly.GetCallingAssembly(), GetCurrentMethod(), message);
         }
 
         public static void Info(string message)
         {
-            SendReport("info", Assembly.GetCallingAssembly(), GetCurrentMethod(), message);
+            EnqueueReport("info", Assembly.GetCallingAssembly(), GetCurrentMethod(), message);
         }
 
         public static void Warning(string message)
         {
-            SendReport("warning", Assembly.GetCallingAssembly(), GetCurrentMethod(), message);
+            EnqueueReport("warning", Assembly.GetCallingAssembly(), GetCurrentMethod(), message);
         }
 
         public static void Error(string message)
         {
-            SendReport("error", Assembly.GetCallingAssembly(), GetCurrentMethod(), message);
+            EnqueueReport("error", Assembly.GetCallingAssembly(), GetCurrentMethod(), message);
         }
 
         public static void Exception(string message, Exception exception)
         {
-            SendReport("exception", Assembly.GetCallingAssembly(), GetCurrentMethod(), message, exception.ToString());
+            EnqueueReport("exception", Assembly.GetCallingAssembly(), GetCurrentMethod(), message, exception.ToString());
         }
 
-        private static void SendReport(string level, Assembly assembly, string culprit, string message, string exception = null)
+        private static void EnqueueReport(string level, Assembly assembly, string culprit, string message, string exception = null)
         {
-            var body = JsonConvert.SerializeObject(new Report(level, assembly, culprit, message, exception.ToString()));
-            webrequests.EnqueuePost(url, body, OnReportSent, null, BuildHeaders());
+            var report = new Report(level, culprit, message, exception);
+            report.DetectModules(assembly);
+            queuedReports.Add(new QueuedReport(report));
+            SubmitNextReport();
         }
 
-        private static void OnReportSent(int code, string response)
+        private static void SubmitNextReport()
         {
+            if (submittingReports || queuedReports.Count < 1) return;
+            var queued_report = queuedReports[0];
+            submittingReports = true;
+            Action<int, string> on_request_complete = (code, response) =>
+            {
+                if (code == 200)
+                {
+                    queuedReports.RemoveAt(0);
+                    submittingReports = false;
+                    SubmitNextReport();
+                }
+                else
+                {
+                    timers.Once(5f, () =>
+                    {
+                        submittingReports = false;
+                        SubmitNextReport();
+                    });
+                }
+            };
+            webrequests.EnqueuePost(url, queued_report.Body, on_request_complete, null, queued_report.Headers);
         }
-        
+
         [MethodImpl(MethodImplOptions.NoInlining)]
         private static string GetCurrentMethod()
         {
