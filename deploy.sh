bcdfcec5d9622e8ac52e49a8c6cfb3ac41518b99
Luke Spragg <luke@spragg.me>
2015-04-05 22:58:47 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,6 @@ git clone -q https://$GITHUB_TOKEN@github.com/OxideMod/Snapshots.git $HOME/Snaps
 
 function bundle_rust {
     cd $HOME/build/$TRAVIS_REPO_SLUG || die_with "Failed to change to project home!"
-    mkdir -p $HOME/temp_rust/RustDedicated_Data/Managed || die_with "Failed to create directory structure!"
     mkdir -p $HOME/temp_rust/RustDedicated_Data/Managed/x64 || die_with "Failed to create directory structure!"
 
     echo "Copying target files to temp directory"
@@ -32,7 +31,6 @@ function bundle_rust {
     Oxide.Ext.Lua/Dependencies/*Lua.dll \
     Oxide.Ext.MySql/Dependencies/*.dll \
     Oxide.Ext.Python/Dependencies/*.dll \
-    Oxide.Ext.SevenDays/Dependencies/System.*.dll \
     Oxide.Ext.SQLite/Dependencies/System.Data.SQLite.dll \
     $HOME/temp_rust/RustDedicated_Data/Managed || die_with "Failed to copy dependency DLLs!"
     cp -vf Oxide.Ext.Lua/Dependencies/x64/*.dll \
@@ -48,13 +46,51 @@ function bundle_rust {
 
     echo "Bundling and compressing target files"
     cd $HOME/temp_rust || die_with "Failed to change to temp directory!"
-    rm -f $HOME/Snapshots/Rust/OxideRust.zip || die_with "Failed to remove old bundle!"
-    zip -FS -vr9 $HOME/Snapshots/Rust/OxideRust.zip . || die_with "Failed to bundle snapshot files!"
+    rm -f $HOME/Snapshots/Oxide-Rust.zip || die_with "Failed to remove old bundle!"
+    zip -FS -vr9 $HOME/Snapshots/Oxide-Rust.zip . || die_with "Failed to bundle snapshot files!"
 } || die_with "Failed to create Rust bundle!"
+
+function bundle_rustlegacy {
+    cd $HOME/build/$TRAVIS_REPO_SLUG || die_with "Failed to change to project home!"
+    mkdir -p $HOME/temp_rustlegacy/rust_server_Data/Managed/x86 || die_with "Failed to create directory structure!"
+
+    echo "Copying target files to temp directory"
+    cp -vf Oxide.Core/bin/Release/Oxide.Core.dll \
+    Oxide.Ext.CSharp/bin/Release/Oxide.Ext.CSharp.dll \
+    Oxide.Ext.JavaScript/bin/Release/Oxide.Ext.JavaScript.dll \
+    Oxide.Ext.Lua/bin/Release/Oxide.Ext.Lua.dll \
+    Oxide.Ext.MySql/bin/Release/Oxide.Ext.MySql.dll \
+    Oxide.Ext.Python/bin/Release/Oxide.Ext.Python.dll \
+    Oxide.Ext.RustLegacy/bin/Release/Oxide.Ext.RustLegacy.dll \
+    Oxide.Ext.SQLite/bin/Release/Oxide.Ext.SQLite.dll \
+    Oxide.Ext.Unity/bin/Release/Oxide.Ext.Unity.dll \
+    $HOME/temp_rustlegacy/rust_server_Data/Managed || die_with "Failed to copy core and extension DLLs!"
+    cp -vf Oxide.Ext.CSharp/Dependencies/Mono.Cecil.dll \
+    Oxide.Ext.JavaScript/Dependencies/Jint.dll \
+    Oxide.Ext.Lua/Dependencies/*Lua.dll \
+    Oxide.Ext.MySql/Dependencies/*.dll \
+    Oxide.Ext.Python/Dependencies/*.dll \
+    Oxide.Ext.SQLite/Dependencies/System.Data.SQLite.dll \
+    $HOME/temp_rustlegacy/rust_server_Data/Managed || die_with "Failed to copy dependency DLLs!"
+    cp -vf Oxide.Ext.Lua/Dependencies/x86/*.dll \
+    Oxide.Ext.SQLite/Dependencies/x86/*.dll \
+    $HOME/temp_rustlegacy/rust_server_Data/Managed/x86 || die_with "Failed to copy dependency DLLs!"
+    cp -vf Oxide.Ext.RustLegacy/Patched/Assembly-CSharp.dll \
+    $HOME/temp_rustlegacy/RustDedicated_Data/Managed || die_with "Failed to copy patched server files!"
+    cp -vf Oxide.Ext.RustLegacy/Patched/oxide.root.json \
+    Oxide.Ext.CSharp/Dependencies/CSharpCompiler.exe \
+    Oxide.Ext.CSharp/Dependencies/mono-2.0.dll \
+    Oxide.Ext.CSharp/Dependencies/msvcr120.dll \
+    $HOME/temp_rustlegacy || die_with "Failed to copy config file and root DLLs!"
+
+    echo "Bundling and compressing target files"
+    cd $HOME/temp_rustlegacy || die_with "Failed to change to temp directory!"
+    rm -f $HOME/Snapshots/Oxide-RustLegacy.zip || die_with "Failed to remove old bundle!"
+    zip -FS -vr9 $HOME/Snapshots/Oxide-RustLegacy.zip . || die_with "Failed to bundle snapshot files!"
+} || die_with "Failed to create Rust Legacy bundle!"
 
 function bundle_7dtd {
     cd $HOME/build/$TRAVIS_REPO_SLUG || die_with "Failed to change to project home!"
-    mkdir -p $HOME/temp_7dtd/7DaysToDie_Data/Managed || die_with "Failed to create directory structure!"
     mkdir -p $HOME/temp_7dtd/7DaysToDie_Data/Managed/x64 || die_with "Failed to create directory structure!"
     mkdir -p $HOME/temp_7dtd/7DaysToDie_Data/Managed/x86 || die_with "Failed to create directory structure!"
 
@@ -94,10 +130,10 @@ function bundle_7dtd {
 
     echo "Bundling and compressing target files"
     cd $HOME/temp_7dtd || die_with "Failed to change to temp directory!"
-    zip -FS -vr9 $HOME/Snapshots/7DaysToDie/Oxide7DaysToDie.zip . || die_with "Failed to bundle snapshot files!"
+    zip -FS -vr9 $HOME/Snapshots/Oxide-7DaysToDie.zip . || die_with "Failed to bundle snapshot files!"
 } || die_with "Failed to create 7 Days to Die bundle!"
 
-bundle_rust; bundle_7dtd
+bundle_rust; bundle_rustlegacy; bundle_7dtd
 
 echo "Adding, committing, and pushing to snapshots"
 cd $HOME/Snapshots || die_with "Failed to change to snapshots directory!"
