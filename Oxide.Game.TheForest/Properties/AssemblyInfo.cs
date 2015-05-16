917d01e59caf4fa0b147068523ecb5c02608d621
Luke Spragg <luke@spragg.me>
2015-05-16 14:40:51 -0400

    [The Forest] Change extension name to Oxide.Game.TheForest

--- Oxide.Game.TheForest/Properties/AssemblyInfo.cs
+++ Oxide.Game.TheForest/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Oxide.Ext.TheForest")]
+[assembly: AssemblyTitle("Oxide.Game.TheForest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Oxide.Ext.TheForest")]
+[assembly: AssemblyProduct("Oxide.Game.TheForest")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
