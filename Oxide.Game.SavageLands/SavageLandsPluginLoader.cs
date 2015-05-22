026a339c47fdac6c61181c23b4895c93c7e9d1ad
Luke Spragg <luke@spragg.me>
2015-05-22 16:39:28 -0400

    [Savage Lands] Stub out extension

---
+++ Oxide.Game.SavageLands/SavageLandsPluginLoader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Oxide.Core.Plugins;
+
+namespace Oxide.Game.SavageLands
+{
+    /// <summary>
+    /// Responsible for loading Savage Lands core plugins
+    /// </summary>
+    public class SavageLandsPluginLoader : PluginLoader
+    {
+        public override Type[] CorePlugins => new[] { typeof(SavageLandsCore) };
+    }
+}
