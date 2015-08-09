6ed516229d291a4092156665163b68d698f9cb3b
Ðie wilde Betty <strikeomania@gmail.com>
2015-08-09 04:13:57 +0200

    [SQLite] rework
    - added async methods
    - removed sync methods
    - added connection checking
    - OpenDb needs plugin reference, optional persistant bool
    - new CloseDb method
    - update sqlite lib

---
+++ Oxide.Ext.SQLite/Connection.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.SQLite;
+using System.Security.Permissions;
+using Oxide.Core.Plugins;
+
+namespace Oxide.Ext.SQLite
+{
+    [ReflectionPermission(SecurityAction.Deny, Flags = ReflectionPermissionFlag.AllFlags)]
+    public sealed class Connection
+    {
+        internal string ConnectionString { get; set; }
+        internal bool ConnectionPersistent { get; set; }
+        internal SQLiteConnection Con { get; set; }
+        internal Plugin Plugin { get; set; }
+
+        public Connection(string connection, bool persistent)
+        {
+            ConnectionString = connection;
+            ConnectionPersistent = persistent;
+        }
+    }
+}
