eb183fb0f193120f25b1af0c4f7ebb295b41d68c
Ðie wilde Betty <strikeomania@gmail.com>
2015-08-09 19:21:56 +0200

    [Core] console status text +1 line and color

--- Oxide.Core/ServerConsole/ServerConsole.cs
+++ Oxide.Core/ServerConsole/ServerConsole.cs
@@ -15,6 +15,8 @@ namespace Oxide.Core.ServerConsole
         public Func<string> Status1Right;
         public Func<string> Status2Left;
         public Func<string> Status2Right;
+        public Func<string> Status3Left;
+        public Func<string> Status3Right;
 
         public Func<string> Title;
 
@@ -24,22 +26,55 @@ namespace Oxide.Core.ServerConsole
             set { _input.Completion = value; }
         }
 
+        public ConsoleColor Status1LeftColor
+        {
+            get { return _input.StatusTextLeftColor[1]; }
+            set { _input.StatusTextLeftColor[1] = value; }
+        }
+        public ConsoleColor Status2LeftColor
+        {
+            get { return _input.StatusTextLeftColor[2]; }
+            set { _input.StatusTextLeftColor[2] = value; }
+        }
+        public ConsoleColor Status3LeftColor
+        {
+            get { return _input.StatusTextLeftColor[3]; }
+            set { _input.StatusTextLeftColor[3] = value; }
+        }
+        public ConsoleColor Status1RightColor
+        {
+            get { return _input.StatusTextRightColor[1]; }
+            set { _input.StatusTextRightColor[1] = value; }
+        }
+        public ConsoleColor Status2RightColor
+        {
+            get { return _input.StatusTextRightColor[2]; }
+            set { _input.StatusTextRightColor[2] = value; }
+        }
+        public ConsoleColor Status3RightColor
+        {
+            get { return _input.StatusTextRightColor[3]; }
+            set { _input.StatusTextRightColor[3] = value; }
+        }
+
         private string status1Left => Status1Left != null ? Status1Left() : "status1left";
         private string status1Right => (Status1Right != null ? Status1Right() : "status1right").PadLeft(_input.LineWidth - 1);
         private string status2Left => Status2Left != null ? Status2Left() : "status2left";
         private string status2Right => (Status2Right != null ? Status2Right() : "status2right").PadLeft(_input.LineWidth - 1);
+        private string status3Left => Status3Left != null ? Status3Left() : "status3left";
+        private string status3Right => (Status3Right != null ? Status3Right() : "status3right").PadLeft(_input.LineWidth - 1);
 
         private string title => Title != null ? Title() : null;
 
-        private static string GetStatus(string left, string right)
+        private static string GetStatusRight(int leftLength, string right)
         {
-            return string.Concat(left, (left.Length >= right.Length ? string.Empty : right.Substring(left.Length)));
+            return leftLength >= right.Length ? string.Empty : right.Substring(leftLength);
         }
 
         public void AddMessage(string message, ConsoleColor color = ConsoleColor.Gray)
         {
             Console.ForegroundColor = color;
-            _input.ClearLine(_input.StatusText.Length);
+            _input.ClearLine(_input.StatusTextLeft.Length);
             Console.WriteLine(message);
             _input.RedrawInputLine();
             Console.ForegroundColor = ConsoleColor.Gray;
@@ -71,7 +106,7 @@ namespace Oxide.Core.ServerConsole
         public static void PrintColoured(params object[] objects)
         {
             if (Interface.Oxide.ServerConsole == null) return;
-            Interface.Oxide.ServerConsole._input.ClearLine(Interface.Oxide.ServerConsole._input.StatusText.Length);
+            Interface.Oxide.ServerConsole._input.ClearLine(Interface.Oxide.ServerConsole._input.StatusTextLeft.Length);
             for (var i = 0; i < objects.Length; i++)
             {
                 if (i%2 != 0)
@@ -104,9 +139,15 @@ namespace Oxide.Core.ServerConsole
             if (_nextUpdate > Interface.Oxide.Now) return;
             _nextUpdate = Interface.Oxide.Now + 0.66f;
             if (!_input.Valid) return;
-            _input.StatusText[0] = string.Empty;
-            _input.StatusText[1] = GetStatus(status1Left, status1Right);
-            _input.StatusText[2] = GetStatus(status2Left, status2Right);
+            string left1 = status1Left, left2 = status2Left, left3 = status3Left;
+            _input.StatusTextLeft[0] = string.Empty;
+            _input.StatusTextLeft[1] = left1;
+            _input.StatusTextLeft[2] = left2;
+            _input.StatusTextLeft[3] = left3;
+            _input.StatusTextRight[0] = string.Empty;
+            _input.StatusTextRight[1] = GetStatusRight(left1.Length, status1Right);
+            _input.StatusTextRight[2] = GetStatusRight(left2.Length, status2Right);
+            _input.StatusTextRight[3] = GetStatusRight(left3.Length, status3Right);
         }
     }
 }
