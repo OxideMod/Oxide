ca2c98c0cd2dca28600d4f8396cfb02d4bd303ce
Luke Spragg <luke@spragg.me>
2015-08-22 23:36:22 -0400

    Minor corrections to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,9 +12,9 @@ echo "Configuring git credentials"
 git config --global user.email "travis@travis-ci.org" || die_with "Failed to configure git user email!"
 git config --global user.name "Travis" || die_with "Failed to configure git user name!"
 
-echo "Cloning snapshots branch using token"
+echo "Cloning Snapshots repo using token"
 GIT_REPO="https://$GITHUB_TOKEN@github.com/OxideMod/Snapshots.git"
-git clone -q --depth 1 $GIT_REPO $HOME/Snapshots >/dev/null || die_with "Failed to clone snapshots repository!"
+git clone -q --depth 1 $GIT_REPO $HOME/Snapshots >/dev/null || die_with "Failed to clone Snapshots repository!"
 
 echo "Bundling and compressing files"
 for d in Bundles/*; do
@@ -23,8 +23,8 @@ for d in Bundles/*; do
     cd $TRAVIS_BUILD_DIR
 done
 
-echo "Adding and comitting snapshots"
-cd $HOME/Snapshots || die_with "Failed to change to snapshots directory!"
+echo "Adding and committing"
+cd $HOME/Snapshots || die_with "Failed to change to Snapshots directory!"
 git add . || die_with "Failed to add files for commit!"
 COMMIT_MESSAGE="Oxide build $TRAVIS_BUILD_NUMBER from https://github.com/$TRAVIS_REPO_SLUG/commit/${TRAVIS_COMMIT:0:7}"
 git commit -m "$COMMIT_MESSAGE" || die_with "Failed to commit files!"
@@ -33,12 +33,12 @@ git config http.postBuffer 52428800
 git config pack.windowMemory "32m"
 git repack --max-pack-size=100M -a -d
 
-echo "Deploying snapshots"
+echo "Deploying to GitHub"
 ATTEMPT=0
 until [ $ATTEMPT -ge 5 ]; do
     git pull && git push -q origin master >/dev/null && break
     ATTEMPT=$[$ATTEMPT+1]
     sleep 15
-done || die_with "Failed to push snapshots to GitHub!"
+done || die_with "Failed to push to GitHub!"
 
 echo "Deployment cycle completed. Happy developing!"
