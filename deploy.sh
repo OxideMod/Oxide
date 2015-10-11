45d09c3e1b79f72ec1f45db5f93938a2ef751dbb
Luke Spragg <luke@spragg.me>
2015-10-10 22:14:19 -0400

    Generalize and update deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+ORG_NAME=${TRAVIS_REPO_SLUG%/*}
+REPO_NAME=${TRAVIS_REPO_SLUG#*/}
+
 function die_with { echo "$*" >&2; exit 1; }
 
 echo "Are you Travis?"
@@ -13,32 +16,37 @@ git config --global user.email "travis@travis-ci.org" || die_with "Failed to con
 git config --global user.name "Travis" || die_with "Failed to configure git user name!"
 
 echo "Cloning Snapshots repo using token"
-GIT_REPO="https://$GITHUB_TOKEN@github.com/OxideMod/Snapshots.git"
-git clone -q --depth 1 $GIT_REPO $HOME/Snapshots >/dev/null || die_with "Failed to clone Snapshots repository!"
-
-echo "Bundling and compressing files"
-for d in Bundles/*; do
-    GAME="${d##*/}"
-    cd Bundles/$GAME && zip -FS -vr9 $HOME/Snapshots/Oxide-$GAME.zip . || die_with "Failed to bundle files!"
-    cd $TRAVIS_BUILD_DIR
-done
-
-echo "Adding and committing"
-cd $HOME/Snapshots || die_with "Failed to change to Snapshots directory!"
-git add . || die_with "Failed to add files for commit!"
-COMMIT_MESSAGE="Oxide build $TRAVIS_BUILD_NUMBER from https://github.com/$TRAVIS_REPO_SLUG/commit/${TRAVIS_COMMIT:0:7}"
-git commit -m "$COMMIT_MESSAGE" || die_with "Failed to commit files!"
-
-git config http.postBuffer 52428800
-git config pack.windowMemory "32m"
-git repack --max-pack-size=100M -a -d
-
-echo "Deploying to GitHub"
+GIT_REPO="https://$GITHUB_TOKEN@github.com/$ORG_NAME/Snapshots.git"
+git clone --depth 1 $GIT_REPO $TRAVIS_BUILD_DIR/Snapshots >/dev/null || die_with "Failed to clone Snapshots repository!"
+cd $TRAVIS_BUILD_DIR/Snapshots || die_with "Failed to change to Snapshots directory!"
+​
 ATTEMPT=0
 until [ $ATTEMPT -ge 5 ]; do
-    git pull && git push -q origin master >/dev/null && break
+    echo "Fetching changes from Snapshots repository"
+    git fetch origin master || die_with "Failed to fetch changes to Snapshots!"
+    git reset --hard origin/master
+
+    echo "Bundling and compressing file(s) to Snapshots"
+    for d in Bundles/*; do
+        GAME="${d##*/}"
+            cp -vf $TRAVIS_BUILD_DIR/$REPO_NAME/bin/Release/$REPO_NAME.exe . || die_with "Failed to copy file(s) to Snapshots!"
+
+        cd $TRAVIS_BUILD_DIR/Bundles/$GAME && zip -FS -vr9 $REPO_NAME-$GAME.zip . || die_with "Failed to create game bundles!"
+    done
+
+    echo "Adding and committing changes"
+    git add . || die_with "Failed to add files for commit!"
+    COMMIT_MESSAGE="$REPO_NAME build $TRAVIS_BUILD_NUMBER from https://github.com/$TRAVIS_REPO_SLUG/commit/${TRAVIS_COMMIT:0:7}"
+    git commit -m "$COMMIT_MESSAGE" || die_with "Failed to commit files!"
+
+    git config http.postBuffer 52428800
+    git config pack.windowMemory "32m"
+    git repack --max-pack-size=100M -a -d
+
+    echo "Pushing build to GitHub"
+    git push -q origin master >/dev/null && break || die_with "Failed to push to GitHub!"
     ATTEMPT=$[$ATTEMPT+1]
     sleep 15
-done || die_with "Failed to push to GitHub!"
+done || die_with "Failed to deploy build!"
 
 echo "Deployment cycle completed. Happy developing!"
