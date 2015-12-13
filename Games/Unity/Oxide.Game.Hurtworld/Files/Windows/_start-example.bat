61e781237403e8587f502f3a4bdc7e35a6df0288
Luke Spragg <luke@spragg.me>
2015-12-13 00:35:57 -0500

    [Hurtworld] Disable OnPlayerRespawn hook for now

--- Games/Unity/Oxide.Game.Hurtworld/Files/Windows/_start-example.bat
+++ Games/Unity/Oxide.Game.Hurtworld/Files/Windows/_start-example.bat
@@ -2,7 +2,7 @@
 cls
 :start
 
-Hurtworld -batchmode -nographics -exec "host 12871;servername My Oxide Server"
+Hurtworld -batchmode -nographics -exec "host 12871;maxplayers 10;servername My Oxide Server"
 
 @echo.
 @echo Restarting server...
