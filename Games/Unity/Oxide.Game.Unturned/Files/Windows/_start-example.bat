7e00cf46557d8b43c687d1191fdab0d4709a90b5
Luke Spragg <luke@spragg.me>
2016-01-01 14:03:45 -0500

    [Unturned] Patch for 3.13.10.0 update

--- Games/Unity/Oxide.Game.Unturned/Files/Windows/_start-example.bat
+++ Games/Unity/Oxide.Game.Unturned/Files/Windows/_start-example.bat
@@ -2,7 +2,7 @@
 cls
 :start
 
-Unturned -batchmode -nographics -name "My Oxide Server" -bind 0.0.0.0 -port 25444 -maxplayers 10 -map pei +secureserver "Oxide"
+Unturned -batchmode -nographics +secureserver "Oxide"
 
 @echo.
 @echo Restarting server...
