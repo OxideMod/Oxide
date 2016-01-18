fb08d859f10146c2c0de097c5cd3ef155fcca4b2
Luke Spragg <luke@spragg.me>
2016-01-18 17:32:23 -0500

    [ReignOfKings] Patch for R3 update

--- Games/Unity/Oxide.Game.ReignOfKings/ReignOfKingsExtension.cs
+++ Games/Unity/Oxide.Game.ReignOfKings/ReignOfKingsExtension.cs
@@ -151,8 +151,8 @@ namespace Oxide.Game.ReignOfKings
         /// <summary>
         /// Loads plugin watchers used by this extension
         /// </summary>
-        /// <param name="plugindir"></param>
-        public override void LoadPluginWatchers(string plugindir)
+        /// <param name="pluginDirectory"></param>
+        public override void LoadPluginWatchers(string pluginDirectory)
         {
         }
 
