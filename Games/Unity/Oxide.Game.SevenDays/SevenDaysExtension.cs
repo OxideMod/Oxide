f476ff0cff60b3fe784e6c183c41b658dcd524e1
Luke Spragg <luke@spragg.me>
2016-01-28 13:14:51 -0500

    [7DaysToDie] Patch for Alpha 13.7 update

--- Games/Unity/Oxide.Game.SevenDays/SevenDaysExtension.cs
+++ Games/Unity/Oxide.Game.SevenDays/SevenDaysExtension.cs
@@ -111,8 +111,8 @@ namespace Oxide.Game.SevenDays
         /// <summary>
         /// Loads plugin watchers used by this extension
         /// </summary>
-        /// <param name="plugindir"></param>
-        public override void LoadPluginWatchers(string plugindir)
+        /// <param name="pluginDirectory"></param>
+        public override void LoadPluginWatchers(string pluginDirectory)
         {
         }
 
