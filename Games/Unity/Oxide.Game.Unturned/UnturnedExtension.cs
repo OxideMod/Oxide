57b458434395bb617d28496d7b8b71de4b47c2fb
Luke Spragg <luke@spragg.me>
2016-01-29 12:57:38 -0500

    [Unturned] Patch for latest updates

--- Games/Unity/Oxide.Game.Unturned/UnturnedExtension.cs
+++ Games/Unity/Oxide.Game.Unturned/UnturnedExtension.cs
@@ -63,8 +63,8 @@ namespace Oxide.Game.Unturned
         /// <summary>
         /// Loads plugin watchers used by this extension
         /// </summary>
-        /// <param name="plugindir"></param>
-        public override void LoadPluginWatchers(string plugindir)
+        /// <param name="pluginDirectory"></param>
+        public override void LoadPluginWatchers(string pluginDirectory)
         {
         }
 
