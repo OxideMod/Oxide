571b3b5142b20e96b310c2face7370fef0ae19a8
Luke Spragg <luke@spragg.me>
2016-02-12 12:36:06 -0500

    [Unturned] Patch for version 3.14.4.0 update

--- Games/Unity/Oxide.Game.Unturned/Libraries/Unturned.cs
+++ Games/Unity/Oxide.Game.Unturned/Libraries/Unturned.cs
@@ -16,6 +16,8 @@ namespace Oxide.Game.Unturned.Libraries
         /// <returns></returns>
         public override bool IsGlobal => false;
 
+        #region Utility
+
         /// <summary>
         /// Gets private bindingflag for accessing private methods, fields, and properties
         /// </summary>
@@ -28,5 +30,7 @@ namespace Oxide.Game.Unturned.Libraries
         /// <param name="str"></param>
         [LibraryFunction("QuoteSafe")]
         public string QuoteSafe(string str) => str.Quote();
+
+        #endregion
     }
 }
