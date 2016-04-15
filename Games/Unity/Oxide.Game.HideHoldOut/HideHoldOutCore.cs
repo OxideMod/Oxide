3a1cdf2bdbdf9f658a7f574d8cafa6959c5f55c6
Luke Spragg <luke@spragg.me>
2016-04-14 21:35:42 -0400

    [HideHoldOut] Add a few new server and player hooks OnPlayerConnected(PlayerInfos player) OnPlayerDisconnected(PlayerInfos player) OnServerInitialized() OnServerShutdown() OnServerSave()

--- Games/Unity/Oxide.Game.HideHoldOut/HideHoldOutCore.cs
+++ Games/Unity/Oxide.Game.HideHoldOut/HideHoldOutCore.cs
@@ -89,5 +89,103 @@ namespace Oxide.Game.HideHoldOut
         private void OnServerShutdown() => Interface.Oxide.OnShutdown();
 
         #endregion
+
+        #region Player Hooks
+
+        /// <summary>
+        /// Called when a user is attempting to connect
+        /// </summary>
+        /// <param name="approval"></param>
+        [HookMethod("IOnUserApprove")]
+        private object IOnUserApprove(uLink.NetworkPlayerApproval approval)
+        {
+            // Get the PlayerInfos
+            var player = FindPlayerById(approval.loginData.ReadString());
+
+            //UnityEngine.Debug.LogWarning(approval.loginData.ReadUInt64().ToString());
+            UnityEngine.Debug.LogWarning(approval.loginData.ReadString());
+            UnityEngine.Debug.LogWarning(player.account_id); // not defined
+            UnityEngine.Debug.LogWarning(player.Nickname); // not defined
+
+            // Reject invalid connections
+            /*if (player.account_id == "0" || string.IsNullOrEmpty(player.Nickname))
+            {
+                approval.Deny(uLink.NetworkConnectionError.ConnectionBanned);
+                return false;
+            }*/
+
+            // Call out and see if we should reject
+            /*var canlogin = Interface.CallHook("CanClientLogin", player);
+            if (canlogin is uLink.NetworkConnectionError)
+            {
+                approval.Deny((uLink.NetworkConnectionError)canlogin);
+                return true;
+            }*/
+
+            return Interface.CallHook("OnUserApprove", approval, player);
+        }
+
+        /// <summary>
+        /// Called when the player has connected
+        /// </summary>
+        /// <param name="netPlayer"></param>
+        [HookMethod("IOnPlayerConnected")]
+        private void IOnPlayerConnected(uLink.NetworkPlayer netPlayer)
+        {
+            // Get the PlayerInfos
+            var player = FindPlayer(netPlayer);
+
+            // Do permission stuff
+            if (permission.IsLoaded)
+            {
+                var userId = player.account_id;
+                permission.UpdateNickname(userId, player.Nickname);
+
+                // Add player to default group
+                if (!permission.UserHasAnyGroup(userId)) permission.AddUserGroup(userId, DefaultGroups[0]);
+            }
+
+            Interface.Oxide.CallHook("OnPlayerConnected", player);
+        }
+
+        /// <summary>
+        /// Called when the player has disconnected
+        /// </summary>
+        /// <param name="netPlayer"></param>
+        [HookMethod("IOnPlayerDisconnected")]
+        private void IOnPlayerDisconnected(uLink.NetworkPlayer netPlayer)
+        {
+            // Get the PlayerInfos
+            var player = FindPlayer(netPlayer);
+
+            Interface.Oxide.CallHook("OnPlayerDisconnected", player);
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        /// <summary>
+        /// Returns the PlayerInfos for the specified uLink.NetworkPlayer
+        /// </summary>
+        /// <param name="player"></param>
+        /// <returns></returns>
+        private PlayerInfos FindPlayer(uLink.NetworkPlayer player) => NetworkController.NetManager_.ServManager.GetPlayerInfos(player);
+
+        /// <summary>
+        /// Returns the PlayerInfos for the specified id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        private PlayerInfos FindPlayerById(ulong id) => NetworkController.NetManager_.ServManager.GetPlayerInfos_accountID(id.ToString());
+
+        /// <summary>
+        /// Returns the PlayerInfos for the specified id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        private PlayerInfos FindPlayerById(string id) => NetworkController.NetManager_.ServManager.GetPlayerInfos_accountID(id);
+
+        #endregion
     }
 }
