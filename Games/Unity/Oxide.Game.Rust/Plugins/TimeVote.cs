1c56858c083f17a6a3412a1d6189b4f27e29c3ee
Nikolai Leesker <N.Leesker@web.de>
2016-04-18 23:10:59 +0200

    init

---
+++ Games/Unity/Oxide.Game.Rust/Plugins/TimeVote.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Oxide.Core;
+using Oxide.Core.Libraries;
+using Oxide.Core.Plugins;
+using Oxide.Game.Rust.Libraries;
+
+namespace Oxide.Plugins
+{
+    [Info("TimeVote", "ThermalCube", 0.1)]
+    [Description("A plugin to show the current time and start votes on a daytime change.")]
+    class TimeVote
+    {
+
+        Oxide.Game.Rust.Libraries.Rust rust = new Oxide.Game.Rust.Libraries.Rust();
+
+        [ChatCommand("time")]
+        void TimeCommand(BasePlayer ply, string cmd, string[] args)
+        {
+            var sky = new TOD_Sky();
+            sky.Cycle.Hour = 6.0f;
+        }
+
+    }
+}
