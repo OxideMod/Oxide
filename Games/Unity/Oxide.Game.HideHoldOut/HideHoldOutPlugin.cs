b9d48b54429f08215d732b2f50e45ada61c007d7
Luke Spragg <luke@spragg.me>
2016-04-24 11:15:30 -0400

    [HideHoldOut] Update HideHoldOutPlugin.cs

--- Games/Unity/Oxide.Game.HideHoldOut/HideHoldOutPlugin.cs
+++ Games/Unity/Oxide.Game.HideHoldOut/HideHoldOutPlugin.cs
@@ -6,13 +6,37 @@ namespace Oxide.Plugins
 {
     public abstract class HideHoldOutPlugin : CSharpPlugin
     {
-        protected HideHoldOut HideHoldOut;
+        protected Command cmd;
+        protected HideHoldOut h2o;
 
         public override void SetPluginInfo(string name, string path)
         {
             base.SetPluginInfo(name, path);
 
-            HideHoldOut = Interface.Oxide.GetLibrary<HideHoldOut>();
+            cmd = Interface.Oxide.GetLibrary<Command>();
+            h2o = Interface.Oxide.GetLibrary<HideHoldOut>("H2o");
+        }
+
+        public override void HandleAddedToManager(PluginManager manager)
+        {
+            foreach (var method in GetType().GetMethods(BindingFlags.NonPublic | BindingFlags.Instance))
+            {
+                var attributes = method.GetCustomAttributes(typeof(ConsoleCommandAttribute), true);
+                if (attributes.Length > 0)
+                {
+                    var attribute = attributes[0] as ConsoleCommandAttribute;
+                    cmd.AddConsoleCommand(attribute?.Command, this, method.Name);
+                    continue;
+                }
+
+                attributes = method.GetCustomAttributes(typeof(ChatCommandAttribute), true);
+                if (attributes.Length > 0)
+                {
+                    var attribute = attributes[0] as ChatCommandAttribute;
+                    cmd.AddChatCommand(attribute?.Command, this, method.Name);
+                }
+            }
+            base.HandleAddedToManager(manager);
         }
     }
 }
