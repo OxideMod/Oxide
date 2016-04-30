3d8136453947431647c1c9e66b47c02ed183b4d9
Nikolai Leesker <N.Leesker@web.de>
2016-04-30 21:50:18 +0200

    fix blueprintrecovery

--- Games/Unity/Oxide.Game.Rust/Plugins/BlueprintRecovery.cs
+++ Games/Unity/Oxide.Game.Rust/Plugins/BlueprintRecovery.cs
@@ -1,5 +1,6 @@
 ﻿using Oxide.Core.Plugins;
 using System;
+using System.Collections.Generic;
 
 namespace Oxide.Plugins
 {
@@ -17,23 +18,30 @@ namespace Oxide.Plugins
         void recover(BasePlayer ply, string cmd, string[] args)
         {
 
-            Item holding = ply.GetActiveItem();
+            List<Item> blueprints = ply.inventory.containerBelt.itemList.FindAll(i => i.IsBlueprint());
 
-            if (!holding.IsBlueprint())
+            if (blueprints.Count == 0)
             {
-                rust.SendChatMessage(ply, "[BP-Recovery]", "Du must ein Blueprint halten um diesen Befehl nutzen zu können!");
+                rust.SendChatMessage(ply, "[BP-Recovery]", "Du must mindestens ein Blueprint halten um diesen Befehl nutzen zu können!");
                 return;
             }
-            Item item = holding;
-            item.SetFlag(Item.Flag.Blueprint, false);
-            ResearchTable rt = new ResearchTable();
-            int frags = (int)Math.Ceiling(Math.Min(Math.Min(rt.BPFragsForFull(item), 200), 1500)*0.75);
-            if (item.info.itemid == 649603450){ frags *= 2; }
 
-            ply.GetActiveItem().Remove(0.0f);
-            ply.GiveItem(ItemManager.CreateByItemID(1351589500, frags));
-            rust.SendChatMessage(ply, "[BP-Recovery]", String.Format("Du hast <color=#008080ff>{0}</color> Blueprint-Fragments erhalten!", frags));
+            blueprints.ForEach(bp =>
+            {
 
+                Item item = bp;
+                item.SetFlag(Item.Flag.Blueprint, false);
+                ResearchTable rt = new ResearchTable();
+                int frags = (int)Math.Ceiling(Math.Min(Math.Min(rt.BPFragsForFull(item), 200), 1500) * 0.75);
+                if (item.info.itemid == 649603450) { frags *= 2; }
+
+                ply.GetActiveItem().Remove(0.0f);
+                ply.GiveItem(ItemManager.CreateByItemID(1351589500, frags));
+                rust.SendChatMessage(ply, "[BP-Recovery]", String.Format("Du hast <color=#008080ff>{0}</color> Blueprint-Fragments erhalten!", frags));
+                
+            });
+
+            
         }
         
     }
