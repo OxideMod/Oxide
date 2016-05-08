e62059fa14ad230f7300512c4c50da8259411e6d
Nikolai Leesker <N.Leesker@web.de>
2016-05-08 16:51:10 +0200

    add: Rust2Text ; add: Sleeping

---
+++ Games/Unity/Oxide.Game.Rust/Plugins/Rust2Text.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oxide.Core.Plugins;
+
+namespace Oxide.Plugins
+{
+    [Info("Rust2Text", "ThermalCube", 0.1)]
+    class Rust2Text : RustPlugin
+    {
+        
+        protected override void LoadDefaultConfig()
+        {
+            PrintWarning("[TimeVote] Creating config file");
+            Config.Clear();
+
+            Config.Settings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            Config.Set("url", "https://stream.watsonplatform.net/speech-to-text/api");
+            Config.Set("credentials", "username", "da6d8184-aefd-4c41-963e-c61b64fdf3d0");
+            Config.Set("credentials", "password", "iAo7eDboi8HS");
+            
+            SaveConfig();
+            string[] sleeping = BasePlayer.activePlayerList.Where(p => p.IsSleeping()).Select(p => p.displayName).ToArray();
+            string msg = "Players sleeping are: " + string.Join(", ", sleeping);
+        }
+        
+    }
+}
---
+++ Games/Unity/Oxide.Game.Rust/Plugins/Sleeping.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oxide.Core.Plugins;
+
+namespace Oxide.Plugins
+{
+    [Info("Sleeping", "ThermalCube", 0.1)]
+    class Sleeping : RustPlugin
+    {
+        [ChatCommand("sleeping")]
+        public void sleeping(BasePlayer ply, string cmd, string[] args)
+        {
+            string[] sleeping = BasePlayer.activePlayerList.Where(p => p.IsSleeping()).Select(p => p.displayName).ToArray();
+            string msg = "Zur Zeit schäft: " + string.Join(", ", sleeping);
+
+            rust.SendChatMessage(ply, "[Sleeping]", msg);
+        }
+        
+    }
+}
