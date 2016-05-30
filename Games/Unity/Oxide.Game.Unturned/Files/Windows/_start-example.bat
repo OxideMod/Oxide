902db477b714bdd2b8a8f35c9ff4f76d5892c73d
Luke Spragg <luke@spragg.me>
2016-05-30 10:24:42 -0400

    [Unturned] Patch for version 3.15.2.1a update

--- Games/Unity/Oxide.Game.Unturned/Files/Windows/_start-example.bat
+++ Games/Unity/Oxide.Game.Unturned/Files/Windows/_start-example.bat
@@ -3,7 +3,7 @@ cls
 :start
 echo Starting server...
 
-Unturned -batchmode -nographics +secureserver "Oxide"
+Unturned.exe -batchmode -nographics -bind 0.0.0.0 -port 27015 -maxplayers 10 -map PEI -name "My Oxide Server" -welcome "Weclome to my Oxide server!" +secureserver "Oxide"
 
 echo.
 echo Restarting server...
