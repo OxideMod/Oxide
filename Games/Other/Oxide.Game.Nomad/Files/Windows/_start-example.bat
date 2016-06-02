f13a9723d232ae2d18da823f1869ec805e0c868a
Luke Spragg <luke@spragg.me>
2016-06-02 14:51:08 -0400

    [Nomad] Patch for version 0.60 update

--- Games/Other/Oxide.Game.Nomad/Files/Windows/_start-example.bat
+++ Games/Other/Oxide.Game.Nomad/Files/Windows/_start-example.bat
@@ -3,9 +3,9 @@ cls
 :start
 echo Starting server...
 
-"Nomad Server\NomadServer.exe" -name "My Oxide Server" -port 5127 -slots 30 -clientVersion "0.581" -password "" -tcpLobby "149.202.51.185" 25565
+"Nomad Server\NomadServer.exe" -name "My Oxide Server" -port 5127 -slots 10 -clientVersion "0.60" -password "" -tcpLobby "149.202.51.185" 25565
 
-@echo.
-@echo Restarting server...
-@echo.
+echo.
+echo Restarting server...
+echo.
 goto start
