3fb0787533e6161c298bd6ffe1e3790a3364e425
Luke Spragg <luke@spragg.me>
2016-10-19 08:56:48 -0400

    [Covalence] Cover our bases with crazy spacing from plugin devs

--- Games/Unity/Oxide.Game.Rust/Libraries/Covalence/RustCommandSystem.cs
+++ Games/Unity/Oxide.Game.Rust/Libraries/Covalence/RustCommandSystem.cs
@@ -113,8 +113,8 @@ namespace Oxide.Game.Rust.Libraries.Covalence
 
             // Setup console command name
             var split = command.Split('.');
-            var parent = split.Length >= 2 ? split[0] : "global";
-            var name = split.Length >= 2 ? string.Join(".", split.Skip(1).ToArray()) : split[0];
+            var parent = split.Length >= 2 ? split[0].Trim() : "global";
+            var name = split.Length >= 2 ? string.Join(".", split.Skip(1).ToArray()) : split[0].Trim();
             var fullname = $"{parent}.{name}";
 
             if (parent == "global") command = name;
