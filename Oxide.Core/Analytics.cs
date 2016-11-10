60d507b0a02f860e5eec5c4019754d37ce2b59cc
Luke Spragg <luke@spragg.me>
2016-11-10 15:48:25 -0500

    [Core] Cleanup and fix a few issues with analytics

--- Oxide.Core/Analytics.cs
+++ Oxide.Core/Analytics.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-
 using Oxide.Core.Libraries;
+using Oxide.Core.Libraries.Covalence;
 using Oxide.Core.Plugins;
 
 namespace Oxide.Core
@@ -12,6 +10,7 @@ namespace Oxide.Core
     {
         private static readonly WebRequests Webrequests = Interface.Oxide.GetLibrary<WebRequests>();
         private static readonly PluginManager PluginManager = Interface.Oxide.RootPluginManager;
+        private static readonly Covalence Covalence = Interface.Oxide.GetLibrary<Covalence>();
         private static readonly Lang Lang = Interface.Oxide.GetLibrary<Lang>();
 
         private const string trackingId = "UA-48448359-3";
@@ -28,31 +27,37 @@ namespace Oxide.Core
             { "dimension1", IntPtr.Size == 8 ? "x64" : "x86" }, // CPU architecture
             { "dimension2", Environment.OSVersion.Platform.ToString().ToLower() }, // OS platform
             { "dimension3", Environment.OSVersion.Version.ToString().ToLower() }, // OS version
-            { "dimension4", Filename.ToLower().Replace("dedicated", "").Replace("server", "").Replace("-", "") }, // Game name
+            { "dimension4", Filename.ToLower().Replace("dedicated", "").Replace("server", "").Replace("-", "").Replace("_", "")  }, // Game name
             { "dimension5", Plugins().Length.ToString() }, // Plugin count
             { "dimension6", string.Join(", ", PluginNames().ToArray()) } // Plugin names
         };*/
 
+        private static readonly string Identifier = $"{Covalence.Server.Address}:{Covalence.Server.Port}";
+
         public static void Collect()
         {
-            var payload = $"v=1&tid={trackingId}&t=screenview&cd={Environment.OSVersion.ToString().Replace("Microsoft ", "")}";
-            payload += $"&cid={Environment.MachineName}{Environment.ProcessorCount}";
-            payload += string.Join("", Environment.GetLogicalDrives()).Replace(":", "").Replace("\\", "").Replace("/", "");
+            var payload = $"v=1&tid={trackingId}&cid={Identifier}&t=screenview&cd=Oxide+for+{Covalence.Game}+{Covalence.Server.Version}";
             payload += $"&an=Oxide&av={OxideMod.Version}&ul={Lang.GetServerLanguage()}";
             //payload += string.Join("&", Tags.Select(kv => kv.Key + "=" + kv.Value).ToArray());
-
             SendPayload(payload);
         }
 
-        public static void Event()
+        public static void Event(string category, string action)
         {
-            // TODO: Complete event sending
+            var payload = $"v=1&tid={trackingId}&cid={Identifier}&t=event&ec={category}&ea={action}";
+            SendPayload(payload);
         }
 
         public static void SendPayload(string payload)
         {
-            var headers = new Dictionary<string, string> {{ "User-Agent", $"Oxide/{OxideMod.Version}" }};
-            Webrequests.EnqueuePost(url, Uri.EscapeUriString(payload), (code, response) => { }, null, headers);
+            var headers = new Dictionary<string, string>
+            {
+                { "User-Agent", $"Oxide/{OxideMod.Version} ({Environment.OSVersion.ToString().Replace("Microsoft ", "")})" }
+            };
+            Webrequests.EnqueuePost(url, Uri.EscapeUriString(payload), (code, response) =>
+            {
+                //Interface.Oxide.LogWarning(response);
+            }, null, headers);
         }
     }
 }
