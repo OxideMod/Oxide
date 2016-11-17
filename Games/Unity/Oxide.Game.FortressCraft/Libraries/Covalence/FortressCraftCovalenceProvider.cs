d22081b1a026846318ab330cd98c2fe8083b9100
Luke Spragg <luke@spragg.me>
2016-11-17 09:19:58 -0500

    [FortressCraft] Add CovalenceProvider

---
+++ Games/Unity/Oxide.Game.FortressCraft/Libraries/Covalence/FortressCraftCovalenceProvider.cs
@@ -0,0 +1,71 @@
+﻿using Oxide.Core.Libraries.Covalence;
+
+namespace Oxide.Game.FortressCraft.Libraries.Covalence
+{
+    /// <summary>
+    /// Provides Covalence functionality for the game "FortressCraft"
+    /// </summary>
+    public class FortressCraftCovalenceProvider : ICovalenceProvider
+    {
+        /// <summary>
+        /// Gets the name of the game for which this provider provides
+        /// </summary>
+        public string GameName => "FortressCraft";
+
+        /// <summary>
+        /// Gets the Steam app ID of the game's client, if available
+        /// </summary>
+        public uint ClientAppId => 254200;
+
+        /// <summary>
+        /// Gets the Steam app ID of the game's server, if available
+        /// </summary>
+        public uint ServerAppId => 254200;
+
+        /// <summary>
+        /// Gets the singleton instance of this provider
+        /// </summary>
+        internal static FortressCraftCovalenceProvider Instance { get; private set; }
+
+        public FortressCraftCovalenceProvider()
+        {
+            Instance = this;
+        }
+
+        /// <summary>
+        /// Gets the player manager
+        /// </summary>
+        public FortressCraftPlayerManager PlayerManager { get; private set; }
+
+        /// <summary>
+        /// Gets the command system provider
+        /// </summary>
+        public FortressCraftCommandSystem CommandSystem { get; private set; }
+
+        /// <summary>
+        /// Creates the game-specific server object
+        /// </summary>
+        /// <returns></returns>
+        public IServer CreateServer() => new FortressCraftServer();
+
+        /// <summary>
+        /// Creates the game-specific player manager object
+        /// </summary>
+        /// <returns></returns>
+        public IPlayerManager CreatePlayerManager() => PlayerManager = new FortressCraftPlayerManager();
+
+        /// <summary>
+        /// Creates the game-specific command system provider object
+        /// </summary>
+        /// <returns></returns>
+        public ICommandSystem CreateCommandSystemProvider() => CommandSystem = new FortressCraftCommandSystem();
+
+        /// <summary>
+        /// Formats the text with markup as specified in Oxide.Core.Libraries.Covalence.Formatter
+        /// into the game-specific markup language
+        /// </summary>
+        /// <param name="text">text to format</param>
+        /// <returns>formatted text</returns>
+        public string FormatText(string text) => Formatter.ToUnity(text);
+    }
+}
