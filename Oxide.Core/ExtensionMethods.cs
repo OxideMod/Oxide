122754cb162fe3b68ec443a51dbe9121e2066ee8
Luke Spragg <luke@spragg.me>
2016-12-08 14:35:47 -0500

    [Core] Revert namespace of ExtensionMethods for now

--- Oxide.Core/ExtensionMethods.cs
+++ Oxide.Core/ExtensionMethods.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace Oxide.Core
+namespace Oxide
 {
     /// <summary>
     /// Useful extension methods which are added to base types
