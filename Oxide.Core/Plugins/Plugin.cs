OxideMod/Oxide

commit 583937dd990c725b5268f17336009b9e7ddbfce5

[Core] Prevent plugin name from being changed after load

Oxide.Core/Plugins/Plugin.cs
@@ -29,7 +29,13 @@ namespace Oxide.Core.Plugins
         /// <summary>
         /// Gets the internal name of this plugin
         /// </summary>
-        public string Name { get; protected set; }
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { if (string.IsNullOrEmpty(Name)) name = value; }
+        }
 
         /// <summary>
         /// Gets the user-friendly title of this plugin
@@ -69,7 +75,13 @@ namespace Oxide.Core.Plugins
         /// <summary>
         /// Gets if this plugin should never be unloaded
         /// </summary>
-        public bool IsCorePlugin { get; set; }
+        private bool isCorePlugin;
+
+        public bool IsCorePlugin
+        {
+            get { return isCorePlugin; }
+            set { if (!Interface.Oxide.HasLoadedCorePlugins) isCorePlugin = value; }
+        }
 
         /// <summary>
         /// Gets the PluginLoader which loaded this plugin
@@ -315,7 +327,8 @@ namespace Oxide.Core.Plugins
                 LoadDefaultConfig();
                 SaveConfig();
             }
-            try { 
+            try
+            {
                 Config.Load();
             }
             catch (Exception ex)

Oxide.Core/OxideMod.cs
@@ -90,7 +90,7 @@ namespace Oxide.Core
         // Allow extensions to register a method to be called every frame
         private Action<float> onFrame;
         private bool isInitialized;
-        private bool hasLoadedCorePlugins;
+        public bool HasLoadedCorePlugins { get; private set; }
 
         public ServerConsole.ServerConsole ServerConsole;
 
@@ -300,9 +300,8 @@ namespace Oxide.Core
             var loaders = extensionManager.GetPluginLoaders();
 
             // Load all core plugins first
-            if (!hasLoadedCorePlugins)
+            if (!HasLoadedCorePlugins)
             {
-                hasLoadedCorePlugins = true;
                 foreach (var loader in loaders)
                 {
                     foreach (var type in loader.CorePlugins)
@@ -319,6 +318,7 @@ namespace Oxide.Core
                         }
                     }
                 }
+                HasLoadedCorePlugins = true;
             }
 
             // Scan the plugin directory and load all reported plugins

commit 108d4613fd6abfef31e7d8364131fd7eb1f074ad

[Core] Fix for Lua and Python plugins Fixes the permission warnings for Lua plugins and main class not found errors for Python plugins caused by https://github.com/OxideMod/Oxide/commit/583937dd990c725b5268f17336009b9e7ddbfce5

Oxide.Core/Plugins/Plugin.cs
@@ -34,7 +34,7 @@ namespace Oxide.Core.Plugins
         public string Name
         {
             get { return name; }
-            set { if (string.IsNullOrEmpty(Name)) name = value; }
+            set { if (string.IsNullOrEmpty(Name) || name == GetType().Name) name = value; }
         }
 
         /// <summary>