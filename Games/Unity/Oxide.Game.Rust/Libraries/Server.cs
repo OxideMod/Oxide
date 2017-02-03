8c6e5e7cca0fc82e049c955aa0f5e0a20ac74ef8
Luke Spragg <luke@spragg.me>
2017-02-03 03:39:04 -0500

    [Rust] Add some WIP library ideas, feedback welcome

---
+++ Games/Unity/Oxide.Game.Rust/Libraries/Server.cs
@@ -0,0 +1,39 @@
+﻿using Oxide.Core.Libraries;
+using Oxide.Core.Libraries.Covalence;
+using Oxide.Game.Rust.Libraries.Covalence;
+
+namespace Oxide.Game.Rust.Libraries
+{
+    public class Server : Library
+    {
+        // Covalence references
+        internal static readonly RustCovalenceProvider Covalence = RustCore.Covalence;
+        internal static readonly IServer ServerInstance = Covalence.CreateServer();
+
+        #region Chat and Commands
+
+        /// <summary>
+        /// Broadcasts a chat message to all players
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="prefix"></param>
+        public void Broadcast(string message, string prefix = null) => ServerInstance.Broadcast(prefix != null ? $"{prefix} {message}" : message);
+
+        /// <summary>
+        /// Broadcasts a chat message to all players
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="prefix"></param>
+        /// <param name="args"></param>
+        public void Broadcast(string message, string prefix = null, params object[] args) => Broadcast(string.Format(message, args), prefix);
+
+        /// <summary>
+        /// Runs the specified server command
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="args"></param>
+        public void Command(string command, params object[] args) => ServerInstance.Command(command, args);
+
+        #endregion
+    }
+}
