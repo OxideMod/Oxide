7466ded15b1b9e36228bcf51e11a960f9e3450f8
Luke Spragg <luke@spragg.me>
2017-02-23 15:23:59 -0500

    [CSharp] Cleanup compiler versions for non-compat OSes

--- Extensions/Oxide.Core.CSharp/CSharpExtension.cs
+++ Extensions/Oxide.Core.CSharp/CSharpExtension.cs
@@ -59,6 +59,8 @@ namespace Oxide.Plugins
             // Register engine frame callback
             Interface.Oxide.OnFrame(OnFrame);
 
+            // Cleanup old files and/or file locations
+            Cleanup.Add(Path.Combine(Interface.Oxide.RootDirectory, Environment.OSVersion.Platform != PlatformID.Unix ? "CSharpCompiler.exe" : "CSharpCompiler.x86"));
             Cleanup.Add(Path.Combine(Interface.Oxide.RootDirectory, "CSharpCompiler"));
             Cleanup.Add(Path.Combine(Interface.Oxide.RootDirectory, "libMonoPosixHelper.so"));
             Cleanup.Add(Path.Combine(Interface.Oxide.RootDirectory, "mono-2.0.dll"));
