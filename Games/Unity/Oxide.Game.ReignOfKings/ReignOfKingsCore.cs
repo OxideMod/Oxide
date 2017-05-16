045c33582a6d306e7794eda37018567329c58848
Luke Spragg <luke@spragg.me>
2017-05-16 15:16:53 -0400

    [ReignOfKings] Fix NullReferenceException when server saves

--- Games/Unity/Oxide.Game.ReignOfKings/ReignOfKingsCore.cs
+++ Games/Unity/Oxide.Game.ReignOfKings/ReignOfKingsCore.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using CodeHatch.Build;
 using CodeHatch.Common;
@@ -45,6 +46,8 @@ namespace Oxide.Game.ReignOfKings
 
         // Track 'load' chat commands
         private readonly Dictionary<string, Player> loadingPlugins = new Dictionary<string, Player>();
+
+        private static readonly FieldInfo FoldersField = typeof(FileCounter).GetField("_folders", BindingFlags.Instance | BindingFlags.NonPublic);
 
         // Commands that a plugin can't override
         internal static IEnumerable<string> RestrictedCommands => new[]
@@ -253,15 +256,15 @@ namespace Oxide.Game.ReignOfKings
         {
             if (fileCounter.FolderLocationFromDataPath.Equals("/Managed/") && fileCounter.Folders.Length != 39)
             {
-                var folders = FileCounter._folders;
+                var folders = (string[])FoldersField.GetValue(fileCounter);
                 Array.Resize(ref folders, 39);
-                folders = fileCounter.Folders;
+                FoldersField.SetValue(fileCounter, folders);
             }
             else if (fileCounter.FolderLocationFromDataPath.Equals("/../") && fileCounter.Folders.Length != 2)
             {
-                var folders = FileCounter._folders;
+                var folders = (string[])FoldersField.GetValue(fileCounter);
                 Array.Resize(ref folders, 2);
-                folders = fileCounter.Folders;
+                FoldersField.SetValue(fileCounter, folders);
             }
         }
 
