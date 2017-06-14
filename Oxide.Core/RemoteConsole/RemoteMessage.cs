4043e4de1f3801239d3cf30308fab03f46a9df44
Luke Spragg <luke@spragg.me>
2017-06-14 14:55:47 -0400

    [Core] Cleanup, fixes, and improvements to RemoteConsole

--- Oxide.Core/RemoteConsole/RemoteMessage.cs
+++ Oxide.Core/RemoteConsole/RemoteMessage.cs
@@ -14,16 +14,27 @@ namespace Oxide.Core.RemoteConsole
         public string Type;
         public string Stacktrace;
 
-        public static RemoteMessage CreateMessage(string message, int ident = 0, string type = "generic", string trace = "") => new RemoteMessage()
+        public static RemoteMessage CreateMessage(string message, int identifier = 0, string type = "generic", string trace = "") => new RemoteMessage()
         {
             Message = message,
-            Identifier = ident,
+            Identifier = identifier,
             Type = type.ToLower(),
             Stacktrace = trace
         };
 
-        public static RemoteMessage GetMessage(string message) => JsonConvert.DeserializeObject<RemoteMessage>(message) ?? null;
-
+        public static RemoteMessage GetMessage(string message)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<RemoteMessage>(message);
+            }
+            catch (JsonReaderException)
+            {
+                Interface.Oxide.LogError("[Rcon] Failed to parse message, incorrect format");
+                return null;
+            }
+        }
+        
         internal string ToJSON() => JsonConvert.SerializeObject(this, Formatting.Indented);
     }
 }
