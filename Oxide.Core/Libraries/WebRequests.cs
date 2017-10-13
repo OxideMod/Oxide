c89b322da5f2dd9c36cdc061891a100475f0d6f9
Luke Spragg <luke@spragg.me>
2017-10-13 11:18:21 -0400

    [Core] Cleanup odd spacing in webrequest lib

--- Oxide.Core/Libraries/WebRequests.cs
+++ Oxide.Core/Libraries/WebRequests.cs
@@ -387,7 +387,7 @@ namespace Oxide.Core.Libraries
         /// <param name="headers"></param>
         /// <param name="timeout"></param>
         [LibraryFunction("Enqueue")]
-        public void Enqueue(string url, string body, Action<int, string> callback, Plugin owner, RequestMethod method = RequestMethod.GET, Dictionary < string, string> headers = null, float timeout = 0f)
+        public void Enqueue(string url, string body, Action<int, string> callback, Plugin owner, RequestMethod method = RequestMethod.GET, Dictionary<string, string> headers = null, float timeout = 0f)
         {
             var request = new WebRequest(url, callback, owner) { Method = method.ToString(), RequestHeaders = headers, Timeout = timeout, Body = body };
             lock (syncroot) queue.Enqueue(request);
