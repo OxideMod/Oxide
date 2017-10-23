56a2c16cdcdb4a77151753945c001f500cfd1da0
Luke Spragg <luke@spragg.me>
2017-10-23 02:36:07 -0400

    Expand handling of Steam logins and add more checks

--- Steam.ps1
+++ Steam.ps1
@@ -9,17 +9,26 @@ param (
 )
 
 Clear-Host
+
+# Format game name and set depot ID if provided
 $game_name = $project -Replace "Oxide."
 if ($depot) { $depot = "-depot $depot" }
+
+# Set directory variables and create directories
 $depot_dir = "$PSScriptRoot\Games\Dependencies\.DepotDownloader"
-New-Item $depot_dir -ItemType Directory -Force
 $patch_dir = "$PSScriptRoot\Games\Dependencies\$project"
 if ($branch -ne "public") { $patch_dir = "$patch_dir-$branch" }
-New-Item $patch_dir -ItemType Directory -Force
 $managed_dir = "$patch_dir\$managed"
+New-Item $depot_dir -ItemType Directory -Force
+New-Item $patch_dir -ItemType Directory -Force
 New-Item $managed_dir -ItemType Directory -Force
 
+function Local-Dependencies {
+    # TODO
+}
+
 function Find-Dependencies {
+    # Check if game's project file exists
     if (!(Test-Path "$game_name.csproj")) {
         Write-Host "Could not find a .csproj file for $game_name"
         exit 1
@@ -47,12 +56,21 @@ function Get-Downloader {
     $version = $json.tag_name -Replace '\w+(\d+(?:\.\d+)+)', '$1'
     $release_zip = $json.assets[0].name
 
+    # Check if latest DepotDownloader is already downloaded
     if (!(Test-Path "$depot_dir\$release_zip") -or !(Test-Path "$depot_dir\DepotDownloader.exe")) {
         # Download and extract DepotDownloader
-        Write-Host "Dowloading version $version of DepotDownloader"
+        Write-Host "Downloading version $version of DepotDownloader"
         Invoke-WebRequest $json.assets[0].browser_download_url -Out "$depot_dir\$release_zip"
+
+        # TODO: Compare size and hash of .zip vs. what GitHub has via API
         Write-Host "Extracting DepotDownloader release files"
         Expand-Archive "$depot_dir\$release_zip" -DestinationPath $depot_dir -Force
+
+        if (!(Test-Patch "$depot_dir\DepotDownloader.exe")) {
+            Get-Downloader # TODO: Add infinite loop prevention
+            return
+        }
+
         # TODO: Cleanup old version .zip file(s)
         #Remove-Item "$depot_dir\depotdownloader-*.zip" -Exclude "$depot_dir\$release_zip" -Verbose –Force
     } else {
@@ -63,16 +81,28 @@ function Get-Downloader {
 function Get-Dependencies {
     # TODO: Check for and compare Steam buildid before downloading again
 
+    # Check if Steam login information is required or not
     if ($access.ToLower() -ne "anonymous") {
-        $steam_login = Get-Content "$PSScriptRoot\.steamlogin"
-        if ($steam_login.Length -ne 2) {
-            Write-Host "Steam username AND password not set in .steamlogin file"
+        if (Test-Path "$PSScriptRoot\.steamlogin") {
+            $steam_login = Get-Content "$PSScriptRoot\.steamlogin"
+            if ($steam_login.Length -ne 2) {
+                Write-Host "Steam username AND password not set in .steamlogin file"
+                exit 1
+            } else {
+                $login = "-username $($steam_login[0]) -password $($steam_login[1])"
+            }
+        } elseif ($env:STEAM_USERNAME -and $env:STEAM_PASSWORD) {
+            $login = "-username $env:STEAM_USERNAME -password $env:STEAM_PASSWORD"
+        } else {
+            Write-Host "No Steam credentials found, skipping build for $game"
             exit 1
         }
-
-        $login = "-username $($steam_login[0]) -password $($steam_login[1])"
     }
 
+    # Cleanup existing game files, else they aren't always the latest
+    #Remove-Item $managed_dir -Include *.dll, *.exe -Exclude "Oxide.Core.dll" -Verbose –Force
+
+    # Attempt to run DepotDownloader to get game DLLs
     try {
         Start-Process "$depot_dir\DepotDownloader.exe" -ArgumentList "$login -app $appid -branch $branch $depot -dir $patch_dir -filelist $patch_dir\.references" -NoNewWindow -Wait
     } catch {
@@ -99,7 +129,7 @@ function Get-Patcher {
     # TODO: MD5 comparision of local OxidePatcher.exe and remote header
     if (!(Test-Path "$managed_dir\OxidePatcher.exe")) {
         # Download latest Oxide Patcher build
-        Write-Host "Dowloading latest build of OxidePatcher"
+        Write-Host "Downloading latest build of OxidePatcher"
         $patcher_url = "https://github.com/OxideMod/OxidePatcher/releases/download/latest/OxidePatcher.exe"
         # TODO: Only download patcher once in $patch_dir, then copy to $managed_dir for each game
         Invoke-WebRequest $patcher_url -Out "$managed_dir\OxidePatcher.exe"
@@ -109,12 +139,13 @@ function Get-Patcher {
 }
 
 function Start-Patcher {
+    # Check if we need to get the Oxide patcher
     if (!(Test-Path "$managed_dir\OxidePatcher.exe")) {
-        Get-Patcher
+        Get-Patcher # TODO: Add infinite loop prevention
         return
     }
 
-    # Patch game using OxidePatcher.exe
+    # Attempt to patch game using the Oxide patcher
     try {
         $opj_name = "$PSScriptRoot\Games\$project\$game_name"
         if ($branch -ne "public") { $opj_name = "$opj_name-$branch" }
@@ -126,8 +157,13 @@ function Start-Patcher {
     }
 }
 
-Find-Dependencies
-Get-Downloader
-Get-Dependencies
+# Check if game is a Steam game or not
+if ($access.ToLower() -eq "nosteam") {
+    Local-Dependencies
+} else {
+    Find-Dependencies
+    Get-Downloader
+    Get-Dependencies
+}
 Get-Patcher
 Start-Patcher
