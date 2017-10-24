c01bd36845c349580783a19a71b5750148cc627a
Luke Spragg <luke@spragg.me>
2017-10-24 00:02:28 -0400

    [FromTheDepths] Update to Visual Studio 2017 project format

---
+++ Games/Oxide.FromTheDepths/FromTheDepthsPluginLoader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Oxide.Core.Plugins;
+
+namespace Oxide.Game.FromTheDepths
+{
+    /// <summary>
+    /// Responsible for loading From the Depths core plugins
+    /// </summary>
+    public class FromTheDepthsPluginLoader : PluginLoader
+    {
+        public override Type[] CorePlugins => new[] { typeof(FromTheDepthsCore) };
+    }
+}
