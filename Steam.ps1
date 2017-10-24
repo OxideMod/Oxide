707fa7e2ce8326a27646962d3d13cc7eadd643b3
Luke Spragg <luke@spragg.me>
2017-10-23 23:13:35 -0400

    Add some more try/catch to Steam script, and cleanup a bit

--- Steam.ps1
+++ Steam.ps1
@@ -15,52 +15,64 @@ $game_name = $project -Replace "Oxide."
 if ($depot) { $depot = "-depot $depot" }
 
 # Set directory variables and create directories
-$depot_dir = "$PSScriptRoot\Games\Dependencies\.DepotDownloader"
-$patch_dir = "$PSScriptRoot\Games\Dependencies\$project"
-if ($branch -ne "public") { $patch_dir = "$patch_dir-$branch" }
+$root_dir = $PSScriptRoot
+$deps_dir = "$root_dir\Games\Dependencies"
+$depot_dir = "$deps_dir\.DepotDownloader"
+$patch_dir = if ($branch -ne "public") { "$deps_dir\$project-$branch" } else { "$deps_dir\$project" }
 $managed_dir = "$patch_dir\$managed"
-New-Item $depot_dir -ItemType Directory -Force
-New-Item $patch_dir -ItemType Directory -Force
-New-Item $managed_dir -ItemType Directory -Force
-
-function Local-Dependencies {
-    # TODO
-}
+New-Item $depot_dir $patch_dir $managed_dir -ItemType Directory -Force
 
 function Find-Dependencies {
-    # Check if game's project file exists
+    # Check if project file exists for game
     if (!(Test-Path "$game_name.csproj")) {
         Write-Host "Could not find a .csproj file for $game_name"
         exit 1
     }
 
-    # Get project information from .csproj file
-    $csproj = Get-Item "$game_name.csproj"
-    $xml = [xml](Get-Content $csproj)
-    Write-Host "Getting references for $branch branch of $appid"
-    try {
-        # TODO: Exclude dependencies included in repository
-        $hint_path = "\.\.\\Dependencies\\\$\(PackageId\)\\\$\(ManagedDir\)\\"
-        ($xml.selectNodes("//Reference") | Select-Object HintPath -ExpandProperty HintPath | Out-String) -Replace $hint_path | Out-File "$patch_dir\.references"
-    } catch {
-        Write-Host "Failed to get references or none found in $game_name.csproj"
-        Write-Host $_.Exception.Message
-        exit 1
+    # Check if Steam is used for game dependencies
+    if ($access.ToLower() -eq "nosteam") {
+        # TODO: Local dependencies handling
+    } else {
+        # Get project information from .csproj file
+        $csproj = Get-Item "$game_name.csproj"
+        $xml = [xml](Get-Content $csproj)
+        Write-Host "Getting references for $branch branch of $appid"
+        try {
+            # TODO: Exclude dependencies included in repository
+            $hint_path = "\.\.\\Dependencies\\\$\(PackageId\)\\\$\(ManagedDir\)\\"
+            ($xml.selectNodes("//Reference") | Select-Object HintPath -ExpandProperty HintPath | Out-String) -Replace $hint_path | Out-File "$patch_dir\.references"
+        } catch {
+            Write-Host "Could not get references or none found in $game_name.csproj"
+            Write-Host $_.Exception.Message
+            exit 1
+        }
     }
 }
 
 function Get-Downloader {
     # Get latest release info for DepotDownloader
     Write-Host "Determining latest release of DepotDownloader"
-    $json = (Invoke-WebRequest "https://api.github.com/repos/SteamRE/DepotDownloader/releases" | ConvertFrom-Json)[0]
-    $version = $json.tag_name -Replace '\w+(\d+(?:\.\d+)+)', '$1'
-    $release_zip = $json.assets[0].name
+    try {
+        $json = (Invoke-WebRequest "https://api.github.com/repos/SteamRE/DepotDownloader/releases" | ConvertFrom-Json)[0]
+        $version = $json.tag_name -Replace '\w+(\d+(?:\.\d+)+)', '$1'
+        $release_zip = $json.assets[0].name
+    } catch {
+        Write-Host "Could not get DepotDownloader information from GitHub"
+        Write-Host $_.Exception.Message
+        exit 1
+    }
 
     # Check if latest DepotDownloader is already downloaded
     if (!(Test-Path "$depot_dir\$release_zip") -or !(Test-Path "$depot_dir\DepotDownloader.exe")) {
         # Download and extract DepotDownloader
         Write-Host "Downloading version $version of DepotDownloader"
-        Invoke-WebRequest $json.assets[0].browser_download_url -Out "$depot_dir\$release_zip"
+        try {
+            Invoke-WebRequest $json.assets[0].browser_download_url -Out "$depot_dir\$release_zip"
+        } catch {
+            Write-Host "Could not download DepotDownloader from GitHub"
+            Write-Host $_.Exception.Message
+            exit 1
+        }
 
         # TODO: Compare size and hash of .zip vs. what GitHub has via API
         Write-Host "Extracting DepotDownloader release files"
@@ -83,8 +95,8 @@ function Get-Dependencies {
 
     # Check if Steam login information is required or not
     if ($access.ToLower() -ne "anonymous") {
-        if (Test-Path "$PSScriptRoot\.steamlogin") {
-            $steam_login = Get-Content "$PSScriptRoot\.steamlogin"
+        if (Test-Path "$root_dir\.steamlogin") {
+            $steam_login = Get-Content "$root_dir\.steamlogin"
             if ($steam_login.Length -ne 2) {
                 Write-Host "Steam username AND password not set in .steamlogin file"
                 exit 1
@@ -119,20 +131,33 @@ function Get-Dependencies {
         # Grab latest Oxide.Core.dll build
         Write-Host "Grabbing latest build of Oxide.Core.dll"
         #$core_version = Get-ChildItem -Directory $core_path | Where-Object { $_.PSIsContainer } | Sort-Object CreationTime -desc | Select-Object -f 1
-        Copy-Item "..\..\Oxide.Core\bin\Release\$dotnet\Oxide.Core.dll" $managed_dir -Force
-        # TODO: Copy websocket-csharp.dll to Dependencies\*Managed
+        try {
+            Copy-Item "..\..\Oxide.Core\bin\Release\$dotnet\Oxide.Core.dll" $deps_dir -Force
+            Copy-Item "..\..\Oxide.Core\bin\Release\$dotnet\Oxide.Core.dll" $managed_dir -Force
+        } catch {
+            Write-Host "Could not copy Oxide.Core.dll from Dependencies\$managed"
+            Write-Host $_.Exception.Message
+            exit 1
+        }
     }
-    # TODO: Return and error if Oxide.Core.dll still doesn't exist
+
+    # TODO: Copy websocket-csharp.dll to Dependencies\*Managed
 }
 
 function Get-Patcher {
     # TODO: MD5 comparision of local OxidePatcher.exe and remote header
-    if (!(Test-Path "$managed_dir\OxidePatcher.exe")) {
+    if (!(Test-Path "$deps_dir\OxidePatcher.exe")) {
         # Download latest Oxide Patcher build
         Write-Host "Downloading latest build of OxidePatcher"
         $patcher_url = "https://github.com/OxideMod/OxidePatcher/releases/download/latest/OxidePatcher.exe"
         # TODO: Only download patcher once in $patch_dir, then copy to $managed_dir for each game
-        Invoke-WebRequest $patcher_url -Out "$managed_dir\OxidePatcher.exe"
+        try {
+            Invoke-WebRequest $patcher_url -Out "$deps_dir\OxidePatcher.exe"
+        } catch {
+            Write-Host "Could not download OxidePatcher.exe from GitHub"
+            Write-Host $_.Exception.Message
+            exit 1
+        }
     } else {
         Write-Host "Latest build of OxidePatcher already downloaded"
     }
@@ -140,16 +165,18 @@ function Get-Patcher {
 
 function Start-Patcher {
     # Check if we need to get the Oxide patcher
-    if (!(Test-Path "$managed_dir\OxidePatcher.exe")) {
+    if (!(Test-Path "$deps_dir\OxidePatcher.exe")) {
         Get-Patcher # TODO: Add infinite loop prevention
         return
     }
 
+    # TODO: Make sure dependencies exist before trying to patch
+
     # Attempt to patch game using the Oxide patcher
     try {
-        $opj_name = "$PSScriptRoot\Games\$project\$game_name"
+        $opj_name = "$root_dir\Games\$project\$game_name"
         if ($branch -ne "public") { $opj_name = "$opj_name-$branch" }
-        Start-Process "$managed_dir\OxidePatcher.exe" -WorkingDirectory $managed_dir -ArgumentList "-c -p $managed_dir $opj_name.opj" -NoNewWindow -Wait
+        Start-Process "$deps_dir\OxidePatcher.exe" -WorkingDirectory $managed_dir -ArgumentList "-c -p $managed_dir $opj_name.opj" -NoNewWindow -Wait
     } catch {
         Write-Host "Could not start or complete OxidePatcher process"
         Write-Host $_.Exception.Message
@@ -158,12 +185,8 @@ function Start-Patcher {
 }
 
 # Check if game is a Steam game or not
-if ($access.ToLower() -eq "nosteam") {
-    Local-Dependencies
-} else {
-    Find-Dependencies
-    Get-Downloader
-    Get-Dependencies
-}
+Find-Dependencies
+Get-Downloader
+Get-Dependencies
 Get-Patcher
 Start-Patcher
