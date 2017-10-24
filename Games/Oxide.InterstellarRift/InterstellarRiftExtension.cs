e68512b426d1e0899640a17b724192083bda2f71
Luke Spragg <luke@spragg.me>
2017-10-23 23:09:57 -0400

    [InterstellarRift] Update to Visual Studio 2017 project format

--- Games/Oxide.InterstellarRift/InterstellarRiftExtension.cs
+++ Games/Oxide.InterstellarRift/InterstellarRiftExtension.cs
@@ -40,7 +40,7 @@ namespace Oxide.Game.InterstellarRift
 
         public override string[] WhitelistAssemblies => new[]
         {
-            "mscorlib", "Oxide.Core", "System", "System.Core"
+            "mscorlib", "System", "System.Core"
         };
         public override string[] WhitelistNamespaces => new[]
         {
