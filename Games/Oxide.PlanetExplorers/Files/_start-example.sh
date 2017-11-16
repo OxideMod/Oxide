04f4a460e097189c4a1f29efeca461745b20c7a8
Luke Spragg <luke@spragg.me>
2017-11-16 11:17:09 -0500

    Split Oxide.PlanetExplorers into separate repository

--- Games/Oxide.PlanetExplorers/Files/_start-example.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
-clear
-while :
-do
-    echo "Starting server...\n"
-    exec ./Server/PE_Server.x86 -batchmode
-    echo "\nRestarting server...\n"
-done
